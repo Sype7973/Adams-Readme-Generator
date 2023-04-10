b9f36410d3f75b62463ff435d6658dc3c607e51a
unknown <adam.day7973@gmail.com>
2023-04-10 15:11:14 +1000

    started homework, added index, and functionality for generate markdown

---
+++ index.js
@@ -0,0 +1,62 @@
+// call to fs and Inquirier
+
+const fs = require('fs');
+const  inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project.',
+    }, 
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions for your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for your project.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+    },  
+]);
+
+function init() {
+    inquirer.prompt(questions)
+      .then((answers) => {
+        const markdown = generateMarkdown(answers);
+        fs.writeFile('README.md', markdown, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Successfully generated README.md');
+          }
+        });
+      });
+  }
+  
+init();
+
+// Function call to initialize app
